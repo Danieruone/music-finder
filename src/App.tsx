c83c3112426e0b835d6023f9ae196071678f3e69
Daniel <danieruone@gmail.com>
2021-08-04 15:39:34 -0500

    http service and search query

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { searchByQuery } from "./services/RestService"
+import { QueryParams } from "./interface/QueryParams" 
 
 function App() {
+  const [input, setInput] = useState("")
+  const [tracks, setTracks] = useState([])
+
+  const handleChange = (e:any) =>{
+    setInput(e.target.value)
+  }
+
+  const handleSubmit = async (e:any) =>{
+    e.preventDefault()
+    const params:QueryParams={
+      q: input,
+      type: "track",
+      market: "US", 
+      limit: 10,
+    } 
+    await searchByQuery(params).then((res:any) => {
+      setTracks(res.data.tracks.items)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Buscador de canciones</h1>
+     <form onSubmit={handleSubmit}>
+       <input type="text" onChange={handleChange}/>
+       <button type="submit">Buscar</button>
+     </form>
+     {tracks.map((track:any, idx) => {
+       return <p key={idx}>{track.name}</p>
+     })}
     </div>
   );
 }
